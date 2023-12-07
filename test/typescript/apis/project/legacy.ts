48d47e285f00037bc0db96c0897d672645989048
Christian Georgi <christian.georgi@sap.com>
2023-12-07 13:35:02 +0100

    More legacy tests

--- test/typescript/apis/project/legacy.ts
+++ test/typescript/apis/project/legacy.ts
@@ -1,7 +1,17 @@
 // verifies typical legacy import patterns
 // through the legacy facade in @sap/cds/apis/...
 
-import { ApplicationService, DatabaseService, QueryAPI, Transaction, Service } from '@sap/cds/apis/services'
-import { Request, User } from '@sap/cds/apis/events'
+import * as cds from '@sap/cds/apis/cds'
+
+import { Association, entity } from '@sap/cds/apis/core'
+import { CREATE, SELECT, Query } from '@sap/cds/apis/cqn'
 import { Definition, CSN } from '@sap/cds/apis/csn'
+import { Request, User } from '@sap/cds/apis/events'
+import { ArrayConstructable, Constructable } from '@sap/cds/apis/internal/inference'
 import { LinkedDefinition } from '@sap/cds/apis/linked'
+import { ConstructedQuery } from '@sap/cds/apis/ql'
+import { ApplicationService, DatabaseService, QueryAPI, Transaction, Service } from '@sap/cds/apis/services'
+
+// works with sap/cds >= 7.5
+// import { User as coreUser } from '@sap/cds/apis/core'
+// import { Logger } from '@sap/cds/apis/log'
