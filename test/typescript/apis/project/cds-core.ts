884dfe1535316bfd58fa4c35e1a2657c621921f6
Daniel O'Grady <daniel.o-grady@sap.com>
2024-01-15 13:34:39 +0100

    Use declaration merging on base classes

--- test/typescript/apis/project/cds-core.ts
+++ test/typescript/apis/project/cds-core.ts
@@ -9,3 +9,14 @@ l1.x
 
 const l2 = cds.lazified({x: 42})
 l2.x
+
+const entity: cds.entity = new cds.entity
+entity.drafts
+const event: cds.event = new cds.event
+event.elements
+const type: cds.type = new cds.type
+type.name
+const array: cds.array = new cds.array
+array.name
+const struct: cds.struct = new cds.struct
+struct.elements
