6af8cf03dfb9b70e872c5a8bc29533f4f543ab63
Christian Georgi <christian.georgi@sap.com>
2024-01-15 17:05:47 +0100

    Test for `builtin...

--- test/typescript/apis/project/cds-core.ts
+++ test/typescript/apis/project/cds-core.ts
@@ -10,13 +10,22 @@ l1.x
 const l2 = cds.lazified({x: 42})
 l2.x
 
-const entity: cds.entity = new cds.entity
-entity.drafts
-const event: cds.event = new cds.event
-event.elements
-const type: cds.type = new cds.type
-type.name
-const array: cds.array = new cds.array
-array.name
-const struct: cds.struct = new cds.struct
-struct.elements
+const e: cds.entity = new cds.entity
+e.drafts
+cds.entity === cds.builtin.classes.entity
+
+const ev: cds.event = new cds.event
+ev.elements
+cds.event === cds.builtin.classes.event
+
+const t: cds.type = new cds.type
+t.name
+cds.type === cds.builtin.classes.type
+
+const arr: cds.array = new cds.array
+arr.name
+cds.array === cds.builtin.classes.array
+
+const s: cds.struct = new cds.struct
+s.elements
+cds.struct === cds.builtin.classes.struct
