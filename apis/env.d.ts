ff36d8d30c20f7bfcbe3660ef6b79c52b3a1c6c2
Arley Triana Morin <arley.triana.morin@sap.com>
2024-02-01 15:20:32 +0100

    formatting

--- apis/env.d.ts
+++ apis/env.d.ts
@@ -15,63 +15,62 @@ export const env: {
   query: any,
   sql: any,
   features: {
+    /**
+     * @see [capire](https://cap.cloud.sap/docs/guides/databases#db-constraints)
+     */
+    assert_integrity: 'off' | 'app' | 'db',
 
-   /**
-    * @see [capire](https://cap.cloud.sap/docs/guides/databases#db-constraints)
-    */
-    assert_integrity: "off" | "app" | "db",
-
-   /**
-    * @see [capire](https://cap.cloud.sap/docs/node.js/cds-log#logging-in-production)
-    */
+    /**
+     * @see [capire](https://cap.cloud.sap/docs/node.js/cds-log#logging-in-production)
+     */
     kibana_formatter: boolean,
 
-   /**
-    * @experimental
-    * @see [capire](https://cap.cloud.sap/docs/advanced/odata#transformations)
-    */
+    /**
+     * @experimental
+     * @see [capire](https://cap.cloud.sap/docs/advanced/odata#transformations)
+     */
     odata_new_parser: boolean,
 
-   /**
-    * @see [capire](https://cap.cloud.sap/docs/releases/archive/2021/oct21#consuming-odata-v2-services-in-node-js)
-    */
+    /**
+     * @see [capire](https://cap.cloud.sap/docs/releases/archive/2021/oct21#consuming-odata-v2-services-in-node-js)
+     */
     cds_tx_inheritance: boolean,
 
-   /**
-    * @see [capire](https://cap.cloud.sap/docs/releases/archive/2022/jun22#optimized-search-on-sap-hana-as-default)
-    */
+    /**
+     * @see [capire](https://cap.cloud.sap/docs/releases/archive/2022/jun22#optimized-search-on-sap-hana-as-default)
+     */
     optimized_search: boolean,
 
-   /**
-    * @see [capire](https://cap.cloud.sap/docs/releases/archive/2022/jun22#new-rest-adapter-as-default)
-    */
+    /**
+     * @see [capire](https://cap.cloud.sap/docs/releases/archive/2022/jun22#new-rest-adapter-as-default)
+     */
     rest_new_adapter: boolean,
 
-   /**
-    * @see [capire](https://cap.cloud.sap/docs/releases/jun23#new-protocol-specific-service-endpoints)
-    */
+    /**
+     * @see [capire](https://cap.cloud.sap/docs/releases/jun23#new-protocol-specific-service-endpoints)
+     */
     serve_on_root: boolean,
 
     with_mocks: boolean,
 
-   /**
-    * @private
-    */
+    /**
+     * @private
+     */
     hybrid_instance_manager: boolean,
 
-   /**
-    * @private
-    */
+    /**
+     * @private
+     */
     keys_in_data_compat: boolean,
 
-   /**
-    * @private
-    */
-    skip_unused: false | "all",
+    /**
+     * @private
+     */
+    skip_unused: false | 'all',
 
-   /**
-    * @private
-    */
+    /**
+     * @private
+     */
     with_parameters: boolean
   }
 } & { [key: string]: any } // to allow additional values we have not yet captured
