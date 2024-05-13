3f73bab3661bd963c4afa61f96b29351ae0001fe
Steffen Waldmann <steffen.waldmann@sap.com>
2024-05-13 10:06:51 +0200

    Types for `cds.env.i18n`

--- apis/env.d.ts
+++ apis/env.d.ts
@@ -7,9 +7,14 @@
 export const env: {
   build: any,
   hana: any,
-  i18n: any,
   requires: any,
   folders: any,
+  i18n: {
+    languages: [string],
+    default_language: string
+    folders: [string],
+    [key: string]: any
+  },
   odata: any,
   query: any,
   sql: any,
