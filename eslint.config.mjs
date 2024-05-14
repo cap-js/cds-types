e4f023316c83fa0f33e4826d02b809d5ab57aaf6
Daniel O'Grady <daniel.o-grady@sap.com>
2024-05-14 18:33:13 +0200

    Disable @typescript-eslint/no-empty-object-type rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,7 +48,8 @@ export default tseslint.config(
         }
       }],
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-declaration-merging': 'off'
+      '@typescript-eslint/no-unsafe-declaration-merging': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off'
     }
   }
 )
