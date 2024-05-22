8717ef3ead0835496eaa899dfaf3b398e00e16dd
Daniel O'Grady <daniel.o-grady@sap.com>
2024-05-22 14:50:04 +0200

    Expose linked from facade and CSN via namespace in facade

--- apis/cds.d.ts
+++ apis/cds.d.ts
@@ -7,17 +7,19 @@ export * from './events'
 export * from './utils'
 export * from './cqn'
 export * from './global'
+export * from './linked'
 export { log, debug } from './log'
 export { test } from './test'
-
-// FIXME: sort out what needs to be exported from csn/linked and under which namespace
-// export { Association, CSN, Definition, Extension, Element, EntityElements, FQN, kinds } from './csn'
-// export { Definitions, LinkedCSN, LinkedDefinition, LinkedAssociation, LinkedEntity, Filter, Visitor } from './linked'
 
 // API extractor cannot handle export * as ql from './ql', so split it into an import and an export statement
 import * as ql from './ql'
 export { ql }
 export { QLExtensions } from './ql' // cds-ql.ts test tries to import this from top level? Correct? Or ql.QLExtensions?
+
+import * as csn from './csn'
+// clashes with linked and not really needed for consumers,
+// so only available in namespaced form (same fix for rollup as above)
+export { csn }
 
 // trick to work around "delete" as reserved identifier
 import { Service } from './services'
