4ab01152cc2e28711f161267d64d2a3c21b1922f
Daniel O'Grady <daniel.o-grady@sap.com>
2024-05-23 21:30:40 +0200

    Add patch for ci runs on prerelease versions

---
+++ .github/prerelease-fix.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// Releasing a pre-release version, like 0.x.y-beta.z causes `npm ci` to install
+// the latest version of cds-types from npmjs as dependency of @sap/cds instead,
+// as pre-release suffixes do not satisfy the semver specified in @sap/cds.
+// This causes the tests to run against an outdated version of cds-types, 
+// often resulting in failing tests (and a block of any release).
+// This script patches the dependency in node_modules/@cap/cds to the
+// local version of cds-types, so the tests run against the correct version.
+
+/* eslint-disable @typescript-eslint/no-var-requires*/
+/* eslint-disable no-undef */
+const { existsSync } = require('fs')
+const { rm } = require('fs/promises')
+const path = require('path')
+
+;(async () => {
+    const faultyDep = path.resolve('node_modules', '@sap', 'cds', 'node_modules', '@cap-js', 'cds-types')
+    if (existsSync(faultyDep)) {
+        await rm(faultyDep, { recursive: true });
+    }
+})()
+
