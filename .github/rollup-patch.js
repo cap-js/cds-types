6367813970e7046bf6c819813190df85e81eaded
Daniel O'Grady <daniel.o-grady@sap.com>
2024-07-02 16:19:14 +0200

    Maintain global nature of sap/cds type augmentation

--- .github/rollup-patch.js
+++ .github/rollup-patch.js
@@ -4,10 +4,65 @@
 /* eslint-disable no-undef */
 const { readFile, writeFile } = require('fs/promises')
 
+/** @param {string} src */
+function _getImports (src) {
+  const named = src.matchAll(/import \* as (.*) from '(.*)'/g) ?? []
+  const destructured = src.matchAll(/import { (.*) } from '(.*)'/g) ?? []
+
+  return {
+    named: [...named].map(([,alias, package]) => ({
+      alias,
+      package
+    })),
+    destructured: [...destructured].map(([, props, package]) => ({
+      package,
+      properties: props.split(', ').map(p => p.trim())
+    }))
+  }
+}
+
+/**
+ * As we want to use model augmentation for @sap/cds, we need to keep
+ * this file as a global file. Having imports will automatically convert
+ * the file from being global to a module.
+ * We therefore have to remove all traditional imports and use
+ * type imports instead (see: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-9.html#import-types)
+ * @param {string} src 
+ */
+function replaceImports (src) {
+  const re = id => new RegExp(`\\b${id}\\b(?!\\?)`, 'g')  // match whole word, but not if it's a type assertion (x?: T)
+  const { named, destructured } = _getImports(src)
+
+  // remove module imports
+  src = src.replaceAll(/^\s*import .* from '.*'.*$/gm, '')
+
+  // split to be able to leave out comments
+  let lines = src.split('\n')
+
+  // import * as foo from 'foo'; x: foo.bar
+  // v
+  // x: import('foo').bar
+  for (const { alias, package } of named) {
+    lines = lines.map(l => l.match(/\s*\*/) ? l : l.replaceAll(re(alias), `import('${package}')`))
+  }
+
+  // import { bar } from 'foo'; x: bar
+  // v
+  // x: import('foo').bar
+  for (const { package, properties } of destructured) {
+    for (const prop of properties) {
+      lines = lines.map(l => l.match(/\s*\*/) ? l : l.replaceAll(re(prop), `import('${package}').${prop}`))
+    }
+  }
+  return lines.join('\n')
+}
+
 ;(async () => {
 
   const rollupFile = './dist/cds-types.d.ts'
   let rollup = (await readFile(rollupFile)).toString()
+
+  rollup = replaceImports(rollup)
 
   // fix `delete` keyword as const - api-extractor does not support it
   rollup = rollup
@@ -26,8 +81,7 @@ const { readFile, writeFile } = require('fs/promises')
 
   // put all exports into an augmented module declaration. Remove all "declare" modifiers
   // as they will now already be in an ambient context
-  rollup = rollup
-    .replaceAll('declare ','')
+  //rollup = rollup.replaceAll('declare ','')
   rollup = `declare module '@sap/cds' {\n${rollup}\n}`
 
   await writeFile(rollupFile, rollup)
