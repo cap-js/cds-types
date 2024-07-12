a98cfcb1c8bd4e12f14247e211ce63538c982aad
Jörg Mann <joerg.mann@sap.com>
2024-07-12 16:22:46 +0200

    fix(postinstall): use symlinkJoin and junction on win

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires*/
 /* eslint-disable no-undef */
 const fs = require('node:fs')
+const { platform } = require('node:os')
 const { join } = require('node:path')
 
 if (!process.env.INIT_CWD) return
@@ -11,4 +12,5 @@ const nodeModules = join(process.env.INIT_CWD, 'node_modules')
 if (!fs.existsSync(nodeModules)) return
 const typesDir = join(nodeModules, '@types')
 if (!fs.existsSync(typesDir)) fs.mkdirSync(typesDir)
-fs.symlink(join(nodeModules, '@cap-js', 'cds-types'), join(typesDir, 'sap__cds'), () => {})
+
+fs.symlinkSync(join(nodeModules, '@cap-js/cds-types'), join(typesDir, 'sap__cds'), platform() === 'win32' ? 'junction' : undefined)
