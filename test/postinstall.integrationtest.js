421866b90706db8006688ef1e898f165aa0a4513
Jörg Mann <joerg.mann@sap.com>
2024-07-15 16:07:06 +0200

    clean up

--- test/postinstall.integrationtest.js
+++ test/postinstall.integrationtest.js
@@ -2,10 +2,13 @@ const cp = require('node:child_process')
 const fs = require('node:fs').promises
 const os = require('node:os')
 const path = require('node:path')
+const util   = require('util')
+
+const execAsync = util.promisify(cp.exec)
 
 
 describe('postinstall', () => {
-    jest.setTimeout(30000)
+    jest.setTimeout(40 * 1000)
 
     let tempFolder
 
@@ -19,7 +22,7 @@ describe('postinstall', () => {
 
     test('create symlink correctly', async () => {
         const projectRoot = path.join(__dirname, '..')
-        cp.execSync(`npm i -D ${projectRoot} `, { cwd: tempFolder })
+        await execAsync(`npm i -D ${projectRoot} `, { cwd: tempFolder })
 
         const typesPackageJsonFile = path.join(tempFolder, 'node_modules/@types/sap__cds/package.json')
         const typesPackageJsonFileContent = await fs.readFile(typesPackageJsonFile, 'utf8')
