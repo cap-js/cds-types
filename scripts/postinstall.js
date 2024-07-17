979bf8e4ede2ac1aaa7ae32285914222bdacb540
Jörg Mann <64193442+joergmann@users.noreply.github.com>
2024-07-17 17:38:52 +0200

    fix(postinstall): create symlink as junction on windows (#141)
    Co-authored-by: Christian Georgi <chgeo@users.noreply.github.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,7 +3,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires*/
 /* eslint-disable no-undef */
 const fs = require('node:fs')
+const { platform } = require('node:os')
 const { join } = require('node:path')
+
+const IS_WIN = platform() === 'win32'
 
 if (!process.env.INIT_CWD) return
 // TODO: check if were in a local install
@@ -11,4 +14,5 @@ const nodeModules = join(process.env.INIT_CWD, 'node_modules')
 if (!fs.existsSync(nodeModules)) return
 const typesDir = join(nodeModules, '@types')
 if (!fs.existsSync(typesDir)) fs.mkdirSync(typesDir)
-fs.symlink(join(nodeModules, '@cap-js', 'cds-types'), join(typesDir, 'sap__cds'), () => {})
+
+fs.symlinkSync(join(nodeModules, '@cap-js/cds-types'), join(typesDir, 'sap__cds'), IS_WIN ? 'junction' : undefined)
