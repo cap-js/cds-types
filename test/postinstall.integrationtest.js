b529f03211e4cd3c9aef9a6740ae16718ccc7506
Jörg Mann <joerg.mann@sap.com>
2024-07-19 10:28:59 +0200

    use pack

--- test/postinstall.integrationtest.js
+++ test/postinstall.integrationtest.js
@@ -11,17 +11,7 @@ describe('postinstall', () => {
     jest.setTimeout(40 * 1000) // timeout 40s for windows
 
     const cdsTypesRoot = path.join(__dirname, '..')
-    let packageTgz
     let tempFolder
-
-    beforeAll(async () => {
-        // build dist folder
-        await execAsync('npm run rollup', { cwd: cdsTypesRoot })
-
-        // pack tarball
-        const { stdout: packageName } = await execAsync(`npm pack`, { cwd: cdsTypesRoot })
-        packageTgz = path.join(cdsTypesRoot, packageName.trim())
-    })
 
     beforeEach(async () => {
         tempFolder = await fs.mkdtemp(path.join(os.tmpdir(), 'postinstall-'))
@@ -31,15 +21,10 @@ describe('postinstall', () => {
         await fs.rm(tempFolder, { recursive: true, force: true })
     })
 
-    afterAll(async () => {
-        await fs.rm(packageTgz)
-    })
-
     test('create symlink correctly', async () => {
         const projectFolder = path.join(tempFolder, 'cds_prj')
         await fs.mkdir(projectFolder, { recursive: true, force: true })
 
-        // await execAsync(`npm i -D ${packageTgz}`, { cwd: projectFolder })
         await execAsync(`npm i -D ${cdsTypesRoot}`, { cwd: projectFolder })
 
         let typesPackageJsonFile = path.join(projectFolder, 'node_modules/@types/sap__cds/package.json')
