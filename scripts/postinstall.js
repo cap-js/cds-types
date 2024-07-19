2dd3254f752f98620b501358ce7eb048bdfef04b
Jörg Mann <joerg.mann@sap.com>
2024-07-19 10:15:31 +0200

    use cpSync

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,7 +5,10 @@
 const fs = require('node:fs')
 const { join, relative, dirname } = require('node:path')
 
+const IS_WIN = process.platform === 'win32'
+
 if (!process.env.INIT_CWD) return
+
 // TODO: check if we're in a local install
 const nodeModules = join(process.env.INIT_CWD, 'node_modules')
 if (!fs.existsSync(nodeModules)) return
@@ -15,15 +18,19 @@ if (!fs.existsSync(typesDir)) fs.mkdirSync(typesDir)
 // use a relative target, in case the user moves the project
 const target = join(typesDir, 'sap__cds')
 const src = join(nodeModules, '@cap-js/cds-types')
-const rel = relative(dirname(target), src) // need dirname or we'd land one level above node_modules (one too many "../")
 
-// remove the existing symlink
-try {
-    fs.unlinkSync(target)
-} catch {
-    // symlink did not exist, continue
+if (IS_WIN) {
+    fs.rmSync(target, { recursive: true, force: true })
+    fs.cpSync(join(src, 'dist'), join(target, 'dist'), { recursive: true, force: true })
+    fs.cpSync(join(src, 'scripts'), join(target, 'scripts'), { recursive: true, force: true })
+    fs.copyFileSync(join(src, 'package.json'), join(target, 'package.json'))
+} else {
+    try {
+        // remove the existing symlink
+        fs.unlinkSync(target)
+    } catch {
+        // symlink did not exist, continue
+    }
+    const rel = relative(dirname(target), src) // need dirname or we'd land one level above node_modules (one too many "../")
+    fs.symlinkSync(rel, target)
 }
-
-// 'junction' is needed to make it work on windows, others ignore
-// fs.symlinkSync(rel, target, 'junction')
-fs.symlinkSync(rel, target)
--- test/postinstall.integrationtest.js
+++ test/postinstall.integrationtest.js
@@ -2,7 +2,7 @@ const cp = require('node:child_process')
 const fs = require('node:fs').promises
 const os = require('node:os')
 const path = require('node:path')
-const util = require('util')
+const util = require('node:util')
 
 const execAsync = util.promisify(cp.exec)
 
@@ -10,7 +10,18 @@ const execAsync = util.promisify(cp.exec)
 describe('postinstall', () => {
     jest.setTimeout(40 * 1000) // timeout 40s for windows
 
+    const cdsTypesRoot = path.join(__dirname, '..')
+    let packageTgz
     let tempFolder
+
+    beforeAll(async () => {
+        // build dist folder
+        await execAsync('npm run rollup', { cwd: cdsTypesRoot })
+
+        // pack tarball
+        const { stdout: packageName } = await execAsync(`npm pack`, { cwd: cdsTypesRoot })
+        packageTgz = path.join(cdsTypesRoot, packageName.trim())
+    })
 
     beforeEach(async () => {
         tempFolder = await fs.mkdtemp(path.join(os.tmpdir(), 'postinstall-'))
@@ -20,13 +31,28 @@ describe('postinstall', () => {
         await fs.rm(tempFolder, { recursive: true, force: true })
     })
 
-    test('create symlink correctly', async () => {
-        const projectRoot = path.join(__dirname, '..')
-        await execAsync(`npm i -D ${projectRoot} `, { cwd: tempFolder })
+    afterAll(async () => {
+        await fs.rm(packageTgz)
+    })
 
-        const typesPackageJsonFile = path.join(tempFolder, 'node_modules/@types/sap__cds/package.json')
-        const typesPackageJsonFileContent = await fs.readFile(typesPackageJsonFile, 'utf8')
-        const packageJson = JSON.parse(typesPackageJsonFileContent)
+    test('create symlink correctly', async () => {
+        const projectFolder = path.join(tempFolder, 'cds_prj')
+        await fs.mkdir(projectFolder, { recursive: true, force: true })
+
+        // await execAsync(`npm i -D ${packageTgz}`, { cwd: projectFolder })
+        await execAsync(`npm i -D ${cdsTypesRoot}`, { cwd: projectFolder })
+
+        let typesPackageJsonFile = path.join(projectFolder, 'node_modules/@types/sap__cds/package.json')
+        let typesPackageJsonFileContent = await fs.readFile(typesPackageJsonFile, 'utf8')
+        let packageJson = JSON.parse(typesPackageJsonFileContent)
+        expect(packageJson.name).toBe('@cap-js/cds-types')
+
+        const newProjectFolder = path.join(tempFolder, 'cds_prj_new')
+        await fs.rename(projectFolder, newProjectFolder)
+
+        typesPackageJsonFile = path.join(newProjectFolder, 'node_modules/@types/sap__cds/package.json')
+        typesPackageJsonFileContent = await fs.readFile(typesPackageJsonFile, 'utf8')
+        packageJson = JSON.parse(typesPackageJsonFileContent)
         expect(packageJson.name).toBe('@cap-js/cds-types')
     })
 })
