f4cbc78c606dfa326632eced78a86746016b6c01
Christian Georgi <chgeo@users.noreply.github.com>
2024-09-11 11:13:58 +0200

    Add `cds.app` (#228)
    See https://cap.cloud.sap/docs/node.js/cds-server#cds-app

--- test/typescript/apis/project/cds-server.ts
+++ test/typescript/apis/project/cds-server.ts
@@ -1,5 +1,5 @@
 import cds from '@sap/cds'
-import express from 'express'
+import express, { Application } from 'express'
 
 const h = undefined as unknown as express.RequestHandler
 
@@ -11,3 +11,5 @@ cds.middlewares.add(h, {after: 'foo'})
 cds.middlewares.add(h, {before: 'foo', after: 'foo'})
 // @ts-expect-error
 cds.middlewares.add(h, {after: 'foo', at: 2})
+
+cds.app satisfies Application
