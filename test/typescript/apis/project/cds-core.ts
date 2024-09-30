54d819b839632c4956fadfd1077113f46c8a412b
Daniel Hutzel <daniel.hutzel@sap.com>
2024-09-30 10:18:30 +0200

    chore: Deprecating cds.lazify

--- test/typescript/apis/project/cds-core.ts
+++ test/typescript/apis/project/cds-core.ts
@@ -4,12 +4,6 @@ const x = cds.extend({ a: 42 }).with({ b: 'hello world' })
 const a: number = x.a
 const b: string = x.b
 
-const l1 = cds.lazify({x: 42})
-l1.x
-
-const l2 = cds.lazified({x: 42})
-l2.x
-
 const e: cds.entity = new cds.entity
 e.drafts
 cds.entity === cds.builtin.classes.entity
